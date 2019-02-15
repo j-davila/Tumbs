21a06812c96b563f93f746cb9296b59718d416f5
JOSE DAVILA <5001347671@wce111-03.local>
2019-02-14 20:05:42 -0800

    1. Add new view to display larger picture when user taps on a post. 2. Added logic to pass data to and from views.

--- Tumbs/PhotoDetailsViewController.swift
+++ Tumbs/PhotoDetailsViewController.swift
@@ -7,24 +7,26 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class PhotoDetailsViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var post: [String:Any]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let photos = post["photos"] as? [[String: Any]] {
+            let photo = photos[0]
+            let originalSize = photo["original_size"] as! [String: Any]
+            let urlString = originalSize["url"] as! String
+            let url = URL(string: urlString)
+            
+            imageView.af_setImage(withURL: url!)
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+}
 
 }
--- Tumbs/PhotosViewController.swift
+++ Tumbs/PhotosViewController.swift
@@ -66,14 +66,16 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
 
-    /*
-    // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        let cell = sender as! UITableViewCell
+        
+        let indexPath = tableView.indexPath(for: cell)!
+        let post = posts[indexPath.row]
+        let detailsViewController = segue.destination as! PhotoDetailsViewController
+        detailsViewController.post = post
+        
     }
-    */
 
 }
